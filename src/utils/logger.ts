05b17a1613cc567fc7e8c41a2fca8c133f8cf149
hyperitsme <154733693+hyperitsme@users.noreply.github.com>
2025-10-27 07:15:28 +0000

    Create logger.ts

---
+++ src/utils/logger.ts
@@ -0,0 +1,3 @@
+export const log  = (...a: any[]) => console.log('[KiraAI]', ...a);
+export const warn = (...a: any[]) => console.warn('[KiraAI:WARN]', ...a);
+export const err  = (...a: any[]) => console.error('[KiraAI:ERR]', ...a);
